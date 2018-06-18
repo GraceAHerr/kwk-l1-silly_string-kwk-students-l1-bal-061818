e0d2e3b4666fa0a807d17be8d546b5bb817ca4b9
GraceAHerr <mazyg321@gmail.com>
2018-06-18 19:55:54 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -46,6 +46,5 @@ sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples 
 
 # Swap all `A` for `O`, as well as all `a` for `o`. You need to chain `gsub`s.
 
-sentence = sentence # Add your gsub chain here
-"Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!" = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".gsub("a", "o")("A","O")
+sentence = sentance.gsub("a", "o").gsub("A","O")
 puts sentence
